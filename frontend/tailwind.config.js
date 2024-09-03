8c97f49bdd63453ab96551e2ffedf2b644d6ac33
Vishesh Karan <vishesh.k@somaiya.edu>
2024-09-03 21:53:11 +0530

    fixed tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,j,sx,ts,tsx}", "./public/index.html", "./dist/modular-booking-system-frontend/browser/index.html"],
+  content: ['./src/**/*.html',
+    './src/**/*.js',
+    './public/**/*.html',
+    './public/**/*.js'],
   theme: {
     extend: {},
   },
