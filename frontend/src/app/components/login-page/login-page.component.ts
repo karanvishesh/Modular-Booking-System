77eb1711ef56dfdba534b092d17a8c58a256dd06
Vishesh Karan <vishesh.k@somaiya.edu>
2024-09-05 02:18:45 +0530

    Added loader for login and sign up screen

--- frontend/src/app/components/login-page/login-page.component.ts
+++ frontend/src/app/components/login-page/login-page.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { FormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-login-page',
   standalone: true,
@@ -10,25 +11,31 @@ import { FormsModule } from '@angular/forms';
   styleUrls: ['./login-page.component.css'],
 })
 export class LoginPageComponent {
-  constructor(private router: Router, private authService: AuthService) {}
-
   username: string = '';
   password: string = '';
   showPassword: boolean = false;
+  loading: boolean = false;
+
+  constructor(private router: Router, private authService: AuthService) {}
 
   togglePasswordVisibility(): void {
     this.showPassword = !this.showPassword;
   }
 
   onSubmit(): void {
-    this.authService.login(this.username, this.password).subscribe(() => {
-      this.authService.authStatus$.subscribe((res: any) => {
-        console.log(res);
-        if (res) this.router.navigate(['/home']);
-      });
-    });
+    this.loading = true;
+    this.authService.login(this.username, this.password).subscribe(
+      (next) => {
+        this.authService.authStatus$.subscribe((res: any) => {
+          this.loading = false;
+          if (res) this.router.navigate(['/home']);
+        });
+      },
+      (error) => {
+        this.loading = false;
+      }
+    );
   }
-
 
   routeToSignUp(): void {
     this.router.navigate(['/register']);
--- frontend/src/app/components/register-page/register-page.component.ts
+++ frontend/src/app/components/register-page/register-page.component.ts
@@ -18,17 +18,26 @@ export class RegisterPageComponent {
   email: string = '';
   password: string = '';
   showPassword: boolean = false;
+  loading: boolean = false;
 
   togglePasswordVisibility(): void {
     this.showPassword = !this.showPassword;
   }
 
   onSubmit(): void {
+    this.loading = true;
     this.authService
       .register(this.username, this.email, this.password)
-      .subscribe((res:any) => {
-        this.routeToLogin();
-      });
+      .subscribe(
+        (res: any) => {
+          this.loading = false;
+          this.routeToLogin();
+        },
+        (error) => {
+          this.loading = false; // Stop the loader in case of error
+          console.error('Registration failed', error);
+        }
+      );
   }
 
   routeToLogin(): void {
